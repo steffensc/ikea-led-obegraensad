9af064f5753916106a0330fc091439fe10208178
Steffen <schmelter.steffen@googlemail.com>
2025-04-14 23:14:34 +0200

    draw leading zeros

--- src/plugins/BigClockPlugin.cpp
+++ src/plugins/BigClockPlugin.cpp
@@ -24,17 +24,8 @@ void BigClockPlugin::loop()
       std::vector<int> mm = {(timeinfo.tm_min - timeinfo.tm_min % 10) / 10, timeinfo.tm_min % 10};
       bool leadingZero = (hh.at(0) == 0);
       Screen.clear();
-      if (leadingZero)
-      {
-        hh.erase(hh.begin());
-        Screen.drawBigNumbers(COLS / 2, 0, hh);
-        Screen.drawBigNumbers(0, ROWS / 2, mm);
-      }
-      else
-      {
-        Screen.drawBigNumbers(0, 0, hh);
-        Screen.drawBigNumbers(0, ROWS / 2, mm);
-      }
+      Screen.drawBigNumbers(0, 0, hh);
+      Screen.drawBigNumbers(0, ROWS / 2, mm);
     }
 
     previousMinutes = timeinfo.tm_min;
